00685eeb67e9b68061c606ac01df4d7aae3c7fff
Son Roy Almerol <sonroya.almerol@gmail.com>
2021-12-09 16:58:56 +0800

    integrate apollo and graphql backend

--- components/footer.js
+++ components/footer.js
@@ -11,6 +11,17 @@ import CardHeader from '@mui/material/CardHeader';
 
 import Logo from '../public/logo.png'
 
+import { useQuery, gql } from "@apollo/client";
+
+const QUERY_CATEGORIES = gql`
+  query Categories  {
+    categories {
+      id
+      name
+    }
+  }
+`;
+
 let theme = createTheme({
     typography: {
         fontFamily: [
@@ -34,89 +45,83 @@ let theme = createTheme({
 theme = responsiveFontSizes(theme)
 
 function Footer(){
+    const { data, loading, error } = useQuery(QUERY_CATEGORIES);
+
     return(
         <ThemeProvider theme={theme}>
-        <Box sx={{padding: 5, flexGrow: 1}}>
-            <Grid container spacing={1} columns={{md: 16}} justifyContent="center">
-                <Grid item md={4} justifyContent="center">
-                    <IconButton 
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                    >
-                        <Image src={Logo} alt="logo"/>
-                    </IconButton>
-                </Grid>
-                <Grid container item md={12} justifyContent="center" spacing={2}>
-                    <Grid item md={3}>
-                        <Grid item>
-                            <Typography variant="h5" color="common.black">Explore</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Home</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Videos</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">FB Live</Typography>
-                        </Grid>
+            <Box sx={{padding: 5, flexGrow: 1}}>
+                <Grid container spacing={1} columns={{md: 16}} justifyContent="center">
+                    <Grid item md={4} justifyContent="center">
+                        <IconButton 
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                        >
+                            <Image src={Logo} alt="logo"/>
+                        </IconButton>
                     </Grid>
-                    <Grid item md={3}>
-                        <Grid item>
-                            <Typography variant="h5" color="common.black">Shops</Typography>
+                    <Grid container item md={12} justifyContent="center" spacing={2}>
+                        <Grid item md={3}>
+                            <Grid item>
+                                <Typography variant="h5" color="common.black">Explore</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">Home</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">Videos</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">FB Live</Typography>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Food</Typography>
+                        { !loading ? (
+                            <Grid item md={3}>
+                                <Grid item>
+                                    <Typography variant="h5" color="common.black">Shops</Typography>
+                                </Grid>
+                                { data.categories.map((category) => (
+                                    <Grid item key={category.id}>
+                                        <Typography variant="h6_bold" color="common.black">{category.name}</Typography>
+                                    </Grid>
+                                )) }
+                            </Grid>
+                        ) : null }
+                        <Grid item md={3}>
+                            <Grid item>
+                                <Typography variant="h5" color="common.black">Info</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">About Us</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">FAQs</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">Policies</Typography>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Clothing</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Health</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Home Living</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Services</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid item md={3}>
-                        <Grid item>
-                            <Typography variant="h5" color="common.black">Info</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">About Us</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">FAQs</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Policies</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid item md={3}>
-                        <Grid item>
-                            <Typography variant="h5" color="common.black">Connect</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Facebook</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Twitter</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="h6_bold" color="common.black">Instagram</Typography>
+                        <Grid item md={3}>
+                            <Grid item>
+                                <Typography variant="h5" color="common.black">Connect</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">Facebook</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">Twitter</Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="h6_bold" color="common.black">Instagram</Typography>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-            <Box sx={{margin: 'auto', textAlign: 'center', marginTop: 5}}>
-                <Typography variant="h6" component="div" color="common.black">Copyright 2021 SAMAHAN Central Board | Developed by SAMAHAN Creative Team and SAMAHAN System Development</Typography>
+                <Box sx={{margin: 'auto', textAlign: 'center', marginTop: 5}}>
+                    <Typography variant="h6" component="div" color="common.black">Copyright 2021 SAMAHAN Central Board | Developed by SAMAHAN Creative Team and SAMAHAN System Development</Typography>
+                </Box>
             </Box>
-        </Box>
         </ThemeProvider>
     )
 }
